57c778af819a2f39ce26db3ee0e1af3893891b25
yamaR <yamadar4423@gmail.com>
2026-09-14 22:22:59 +0900

    sw: キャッシュ版を v7 へ

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 一級建築士 学習ノート オフライン用 Service Worker
    公開のたびに VERSION を上げる（tools/publish.sh が自動で上げます） */
-var VERSION = 'v6';
+var VERSION = 'v7';
 var CACHE = 'ikkyu-' + VERSION;
 
 var ASSETS = [
